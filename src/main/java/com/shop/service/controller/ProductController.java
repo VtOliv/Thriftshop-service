e2af4da207a346d2f7f879f07b8630c1cac06370
Vitor de Oliveira <vitorturmac@gmail.com>
2026-04-07 18:37:16 -0300

    feat: basic api for getting all products

--- src/main/java/com/shop/service/controller/ProductController.java
+++ src/main/java/com/shop/service/controller/ProductController.java
@@ -1,5 +1,36 @@
 package com.shop.service.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shop.service.domain.Product;
+import com.shop.service.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
+	
+	private final ProductService productService;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> getAllProducts() {
+		
+		log.info("Received request to get all products");
+		
+		var products = productService.getAllProducts();
+		
+		log.info("Returning {} products", products.size());
+		
+		return ResponseEntity.ok(products);
+	}
 
 }
--- src/main/java/com/shop/service/service/ProductService.java
+++ src/main/java/com/shop/service/service/ProductService.java
@@ -1,5 +1,21 @@
 package com.shop.service.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.shop.service.domain.Product;
+import com.shop.service.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
 
+	private final ProductRepository productRepository;
+	
+	public List<Product> getAllProducts() {
+		return productRepository.findAll();
+	}
 }
